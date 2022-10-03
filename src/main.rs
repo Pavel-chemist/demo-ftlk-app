48d3adaed776b7f948c599f640a1c52b6e6e7396
Pavel-chemist <pavel.chemist@gmail.com>
2022-10-03 22:37:32 +0400

    make base ftlk window work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use fltk::{app, prelude::*, window::Window};
+
 fn main() {
-    println!("This will be a windowed application");
+    let a = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "My first ftlk Window");
+    wind.end();
+    wind.show();
+    a.run().unwrap();
 }
