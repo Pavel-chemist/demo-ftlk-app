4341c8f394e9c268379897280b6871ff94108b1c
Pavel-chemist <pavel.chemist@gmail.com>
2022-10-04 09:46:03 +0400

    make button interactive -- change text on click

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use fltk::{app, prelude::*, window, button};
+use fltk::{app, prelude::*, *};
 
 fn main() {
     let application = app::App::default();
     let mut wind = window::Window::new(256, 128, 512, 256, "My first ftlk Window");
-    let butt = button::Button::new(192, 96, 128, 32, "Click me!");
+
+    let mut frame = frame::Frame::new(10, 10, 200, 50, "Click on button below:");
+    let mut butt = button::Button::new(10, 80, 200, 50, "Click me!");
+
+    butt.set_callback(move |_| frame.set_label("You've clicked on button"));
 
     wind.end();
     wind.show();
